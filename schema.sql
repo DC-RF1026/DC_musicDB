61398f90e00696a060ccefba76e4e8dc4e6e5486
DC-RF1026 <129828685+DC-RF1026@users.noreply.github.com>
2023-05-30 11:52:16 +0900

    add sql files
    SQLファイル更新

---
+++ schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE music(
+id INT PRIMARY KEY AUTOINCREMENT,
+title VARCHAR(255) NOT NULL,
+artist VARCHAR(255) NOT NULL,
+acosticness DECIMAL,
+danceability DECIMAL,
+energy DECIMAL,
+instrumentalness DECIMAL,
+liveness DECIMAL,
+loudness DECIMAL,
+tempo DECIMAL,
+link VARCHAR(255),
+genre VARCHAR(255));
