5c3943cd50093e8dc6f2a98ae082521069807210
DC-RF1026 <129828685+DC-RF1026@users.noreply.github.com>
2023-05-30 11:55:13 +0900

    rename callam
    カラム名をdaoの形に修正しました

--- data.sql
+++ data.sql
@@ -1,5 +1,5 @@
 INSERT INTO
-users(title, artist, acositicness, danceability, energy, instrumentalness, liveness, tempo, link, genre)
+users(song, artist, acositicness, danceability, energy, instrumentalness, liveness, tempo, link, genre)
 VALUSE
 ('透明少女', 'ナンバーガール',0.089, 0.877, 3.16, 0.897, 0.151, 181.798, 'https://open.spotify.com/track/6b5DOkHnZZOnJnhaWXjifT', "Rock"),
 ('Its A Shame', 'The Spinners' 0.372, 0.642, 0.0947, 0.0, 0.117, 198.731, 'https://open.spotify.com/track/1l1YTy9nJ0trwhsCGcimly', "Soul"),
