92a129eb66196b3719d8c8053624fd37d53df8fc
shoya-hayashi <129828937+shoya-hayashi@users.noreply.github.com>
2023-05-30 13:47:33 +0900

    Update MusicController.java
    ファイルアップデート

--- tekitou/MusicController.java
+++ tekitou/MusicController.java
@@ -29,32 +29,49 @@ public class MusicController {
 		return "form/login";
 	}
 
+//index画面 ソート機能を実装した場合
+//	@RequestMapping("/home")
+//	public String sample(Model model, Music music, @RequestParam(value = "sort", required = false) String sort) {
+//		List<EntMusic> list;
+//
+//		if (sort != null && sort.equals("low")) {
+//			// 低い順にソートして取得,dailydaoのsearchDbSortedByDateメソッドの引数にtrueを渡す。
+//			list = musicdao.searchDbSortedBySong(true);
+//		} else if (sort != null && sort.equals("high")) {
+//			// 高い順にソートして取得
+//			list = musicdao.searchDbSortedBySong(false);
+//		} else {
+//			// 通常の取得、ソートボタン押してない時に表示させるもの
+//			list = musicdao.searchDb();
+//		}
+//
+//		return "index";
+//	}
+
 	//index画面
-	@RequestMapping("/{id}/home")
-	public String sample(Model model, User user) {
-		
+	@RequestMapping("/home")
+	public String sample(Model model) {
 
 		return "index";
 	}
 
 	//お気に入り画面　　お気に入り付けた曲を一覧で表示(01で識別？)
-	@RequestMapping("/{id}/favorite")
-	public String favorite(Model model, User user) {
+	@RequestMapping("/favorite")
+	public String favorite(Model model) {
 
 		return "form/favorite";
 	}
-	
 
 	//編集画面
-	@RequestMapping("/{id}/edit")
-	public String edit(Model model, User user) {
+	@RequestMapping("edit")
+	public String edit(Model model) {
 
 		return "form/edit";
 	}
 
 	//更新処理(UPDATE)
-	@RequestMapping("/{id}/edit/{id}/exe")
-	public String editExe(@PathVariable Long id, Model model, Music music, User user) {
+	@RequestMapping("/edit/{id}/exe")
+	public String editExe(@PathVariable Long id, Model model, Music music) {
 		//フォームの値をエンティティに入れ直し
 		//		EntMusic entmusic = new EntMusic();
 		//		entmusic.setName(music.getName());
@@ -64,28 +81,27 @@ public class MusicController {
 		return "redirect:/index";
 	}
 
-	//削除(DELETE)
-	@RequestMapping("/{id}/del/{id}")
-	public String delete(@PathVariable Long id, User user) {
+	//物理削除(DELETE)　　＊論理削除じゃない＊
+	@RequestMapping("/del/{id}")
+	public String delete(@PathVariable Long id) {
 		//		musicdao.deleteDb(id);
 		return "redirect:/index";
 	}
-
+	
 	//検索画面
-	@RequestMapping("/{id}/search")
-	public String search(Model model, User user) {
+	@RequestMapping("/search")
+	public String search(Model model) {
 
 		return "form/search";
 	}
 
-
-//ここから検索画面についての処理
+	//ここから検索画面についての処理
 	//曲名検索(SELECT)
 	@RequestMapping("/search/{song}/")
-	public String searchsong(Music music, Model model, User user) {
-		
-				List<EntMusic> list = musicdao.selectSong(music.getSong());
-				model.addAttribute("songlist", list);
+	public String searchsong(Music music, Model model) {
+
+		List<EntMusic> list = musicdao.selectSong(music.getSong());
+		model.addAttribute("songlist", list);
 		//検索したものでソートをかけ、その内容をindex.htmlに表示
 
 		return "index";
@@ -94,22 +110,22 @@ public class MusicController {
 	//アーティスト検索(SELECT)
 	@RequestMapping("/search/{artist}/")
 	public String searchartist(Music music, Model model, User user) {
-		
-				List<EntMusic> list = musicdao.selectArtist(music.getArtist());
 
-				model.addAttribute("artistlist", list);
+		List<EntMusic> list = musicdao.selectArtist(music.getArtist());
+
+		model.addAttribute("artistlist", list);
 		return "index";
 	}
 
 	//ジャンル検索(SELECT)
 	@RequestMapping("/search/{genre}/")
 	public String searchgenre(Music music, Model model, User user) {
-		
-				List<EntMusic> list = musicdao.selectGenre(music.getGenre());
-		
-				model.addAttribute("genrelist", list);
+
+		List<EntMusic> list = musicdao.selectGenre(music.getGenre());
+
+		model.addAttribute("genrelist", list);
 		return "index";
 	}
-//ここまで
-	
+	//ここまで
+
 }
