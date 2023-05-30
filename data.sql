55742abcff684697d03d8b0ecab001b1b3aadb56
DC-RF1026 <129828685+DC-RF1026@users.noreply.github.com>
2023-05-30 12:07:10 +0900

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,6 +1,6 @@
 INSERT INTO
-users(song, artist, acositicness, danceability, energy, instrumentalness, liveness, tempo, link, genre)
-VALUSE
+(song, artist, acositicness, danceability, energy, instrumentalness, liveness, tempo, link, genre)
+VALUES
 ('透明少女', 'ナンバーガール',0.089, 0.877, 3.16, 0.897, 0.151, 181.798, 'https://open.spotify.com/track/6b5DOkHnZZOnJnhaWXjifT', "Rock"),
 ('Its A Shame', 'The Spinners' 0.372, 0.642, 0.0947, 0.0, 0.117, 198.731, 'https://open.spotify.com/track/1l1YTy9nJ0trwhsCGcimly', "Soul"),
 ('Pop Virus', '星野源', 0.629, 0.441, 0.0339, 0.0, 0.234, 78.177, 'https://open.spotify.com/track/4hQ5tBCwPRkzhFB424U63U', "Pops");
